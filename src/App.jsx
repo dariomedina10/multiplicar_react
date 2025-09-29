32ec281e74cd63583498b691d10e3d32cdbae38e
dariomedina10 <dariomedina10@gmail.com>
2025-09-29 11:27:10 -0400

    Creando Tabla de multiplicar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import react from "react";
 import { useState } from 'react'
 import './App.css'
 
+function tabla_multiplicar(){
+const vector = new Array(10)
+ for (let x = 0; x < vector.length; x++)
+      vec[x] = x *  2
+    setNumeros(vec)
+ const [numeros, setNumeros] = useState([0, 0, 0, 0, 0,0, 0, 0, 0, 0]);
+
+ return (
+    <div>
+      <p>Números aleatorios:</p>
+      {vector.map(num => (<p>{num}</p>))}
+      <button onClick={tabla_multiplicar}>Generar Tabla de Multiplicar</button>
+    </div>
+  );
+}
+}
+
 function App() {
+  <tabla_multiplicar/>
   const [count, setCount] = useState(0)
 
   return (
