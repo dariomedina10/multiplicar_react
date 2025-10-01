77b541714b88d72ed6701300c4a6ee9b83950adc
dariomedina10 <dariomedina10@gmail.com>
2025-10-01 01:56:43 -0400

    Creando Tabla de multiplicar DEL 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,55 @@
-import react from "react";
+import React from "react";
 import { useState } from 'react'
+
 import './App.css'
 
-function tabla_multiplicar(){
-const vector = new Array(10)
- for (let x = 0; x < vector.length; x++)
+
+
+function App() {
+const [numeros, setNumeros] = useState([0, 0, 0, 0, 0,0, 0, 0, 0, 0]);
+  function tabla_multiplicar(){
+const vec = new Array(10)
+ for (let x = 0; x < vec.length; x++)
       vec[x] = x *  2
     setNumeros(vec)
- const [numeros, setNumeros] = useState([0, 0, 0, 0, 0,0, 0, 0, 0, 0]);
+}
+ 
 
  return (
     <div>
-      <p>Números aleatorios:</p>
-      {vector.map(num => (<p>{num}</p>))}
+      <p>Vector de 10 posiciones con tabla de multiplicar:</p>
+      {numeros.map(num => (<p>{num}</p>))}
       <button onClick={tabla_multiplicar}>Generar Tabla de Multiplicar</button>
     </div>
   );
 }
-}
-
-function App() {
-  <tabla_multiplicar/>
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-      
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-    
-      </div>
-      <p className="read-the-docs">
-        Darío Fernando Trabajando...
-      </p>
-    </>
-  )
-}
 
 export default App
+
+  
+  //const [count, setCount] = useState(0)
+
+  // return (
+  //   <>
+  //     <div>
+      
+  //     </div>
+  //     <h1>Vite + React</h1>
+  //     <div className="card">
+  //       <button onClick={() => setCount((count) => count + 1)}>
+  //         count is {count}
+  //       </button>
+    
+  //     </div>
+  //     <p className="read-the-docs">
+  //       Darío Fernando Trabajando...
+  //     </p>
+  //   </>
+  // )
+
+
+
+
+
+
+
